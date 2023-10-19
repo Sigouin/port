6a1d74e69001b02512e402a97cea38a324aeac0a
Matthew <mattsigpip@gmail.com>
2023-10-19 12:33:42 -0400

    Added some folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { About } from "./components/About/about";
+import { Contact } from "./components/Contact/contact";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Skills } from "./components/Skills/Skills";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Contact/contact.tsx
@@ -0,0 +1,3 @@
+export function Contact() {
+  return <div>Contact Info</div>;
+}
