48aadd1f01254604eb295e89fdb29968a381a2dd
Matthew <mattsigpip@gmail.com>
2023-10-21 16:48:42 -0400

    Finished up portfolio component and styling

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,31 +1,45 @@
 import "./portfolio.css";
 
+// Fill array with projects that need to be displayed
+
+// const data = [
+//   {
+//     id: 1,
+//     image:
+//     title:
+//     github:
+//     demo:
+//   },
+// ]
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-
+      {/* data.map(({id, image, title, github, demo}) => {}) */}
       <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__item-image"></div>
           <h3>This is a portfolio item title</h3>
-          <a
-            href="https://github.com"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-          <a
-            href="https://github.com"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
