8ab14144444978f1f56a00375aac1aa09848fcb7
Matthew <mattsigpip@gmail.com>
2023-10-24 11:25:38 -0400

    Added LinkedIn/Github links Fixed css navbar color

--- src/components/Header/HeaderSocials.tsx
+++ src/components/Header/HeaderSocials.tsx
@@ -3,10 +3,14 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+      <a
+        href="https://linkedin.com/in/matt-sigouin"
+        target="_blank"
+        rel="noreferrer"
+      >
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank" rel="noreferrer">
+      <a href="https://github.com/Sigouin" target="_blank" rel="noreferrer">
         <BsGithub />
       </a>
     </div>
